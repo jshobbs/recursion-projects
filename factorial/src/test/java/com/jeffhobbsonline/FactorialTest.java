ceadf69565a20a54c8552da2368122bfac6aa679
Jeff Hobbs <jshobbssocal@gmail.com>
2016-09-07 22:28:55 -0700

    Add test for zero and negative number.
    Change naming convention for tests.

--- factorial/src/test/java/com/jeffhobbsonline/FactorialTest.java
+++ factorial/src/test/java/com/jeffhobbsonline/FactorialTest.java
@@ -30,30 +30,35 @@ public class FactorialTest
     }
 
     /**
-     * Rigourous Test :-)
-     */
-    public void testOne()
+     * Unit Tests
+     * 
+    */
+    public void testNegativeInteger()
+    {
+    	Assert.assertEquals(-4, Factorial.factorial(-4));
+    } 
+    public void testIntegerZero()
+    {
+    	Assert.assertEquals(0, Factorial.factorial(0));
+    } 
+    public void testIntegerOne()
     {
     	Assert.assertEquals(1, Factorial.factorial(1));
     } 
-    public void testTwo()
+    public void testIntegerTwo()
     {
     	Assert.assertEquals(2, Factorial.factorial(2));
     } 
-    public void testFour()
+    public void testIntegerFour()
     {
         Assert.assertEquals(24, Factorial.factorial(4));
     } 
-    public void testSix()
+    public void testIntegerSix()
     {
         Assert.assertEquals(720, Factorial.factorial(6));
     } 
-    public void testTen()
+    public void testIntegerTen()
     {
     	Assert.assertEquals(3628800, Factorial.factorial(10));
-    }
-    public void testTwelve()
-    {
-    	Assert.assertEquals(479001600, Factorial.factorial(12));
     }
 }
